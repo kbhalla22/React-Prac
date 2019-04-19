e8e68dd2557067d4dbd6b7d4667830be90adbc55
karanbhalla <kbhalla22@gmail.com>
2019-04-19 16:31:28 -0400

    made changes to the file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, i am a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name 1</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
